2c1cabec16b750ab048ac23bca1502122a5030f2
hyunjun <hyunjun5681@kakao.com>
2025-05-02 22:06:23 +0900

    Greatest Common Divisor of Strings 링크 추가

--- src/main/java/com/leetcode/GreatestCommonDivisorOfStrings.java
+++ src/main/java/com/leetcode/GreatestCommonDivisorOfStrings.java
@@ -1,5 +1,9 @@
 package com.leetcode;
 
+/**
+ * https://leetcode.com/problems/greatest-common-divisor-of-strings/?envType=study-plan-v2&envId=leetcode-75
+ */
+
 public class GreatestCommonDivisorOfStrings {
 
     public static void main(String[] args) {
