4b48a830a20489eb650a294d3c01d971d5d02eed
hyunjun <hyunjun5681@kakao.com>
2025-05-31 22:31:11 +0900

    Keys and Rooms 추가

---
+++ src/main/java/com/leetcode/KeysAndRooms.java
@@ -0,0 +1,60 @@
+package com.leetcode;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+
+public class KeysAndRooms {
+
+    public static void main(String[] args) {
+        List<List<Integer>> rooms = List.of(
+            List.of(1),
+            List.of(2),
+            List.of(3),
+            List.of()
+        );
+
+//        List<List<Integer>> rooms = List.of(
+//                List.of(1,3),
+//                List.of(3,0,1),
+//                List.of(2),
+//                List.of(0)
+//        );
+
+        boolean result = canVisitAllRooms(rooms);
+        System.out.println(result);
+    }
+
+    public static boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < rooms.size(); i++){  // 방문 가능한 전체 방 번호 Set
+            set.add(i);
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);  // start room(0번 방)
+        boolean[] visitLog = new boolean[set.size()+1];     // 0번 방 포함
+
+        while(!stack.isEmpty()){
+            Integer currRoom = stack.pop();
+            visitLog[currRoom] = true;      // 현재 방 방문여부 수정
+
+            if(set.contains(currRoom)){     // 현재 방이 방문한 방인 경우
+                set.remove(currRoom);
+            }
+
+            List<Integer> currKeys = rooms.get(currRoom);   // 현재 방에 있는 키 목록
+
+            for(Integer key : currKeys){
+                if(!visitLog[key]){     // 아직 방문하지 않은 방인 경우 방 키 추가
+                    stack.push(key);
+                }
+            }
+        }
+
+        boolean result = set.isEmpty() ? true : false;  // 모든 방 방문여부 확인
+        return result;
+    }
+}
