a5e8db908ae7c86352edc9b9405357126c8c6d60
hyunjun <hyunjun5681@kakao.com>
2025-06-01 11:44:45 +0900

    Smallest Number in Infinite Set 추가

---
+++ src/main/java/com/leetcode/SmallestInfiniteSet.java
@@ -0,0 +1,35 @@
+package com.leetcode;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+
+public class SmallestInfiniteSet {
+
+    private Queue<Integer> queue;
+    private Set<Integer> set;
+
+    public SmallestInfiniteSet() {
+        queue = new PriorityQueue<>();
+        set = new HashSet<>();
+
+        for(int i = 1 ; i <= 1000 ; i++){
+            queue.add(i);
+            set.add(i);
+        }
+    }
+
+    public int popSmallest() {
+        int val = queue.poll();
+        set.remove(val);
+        return val;
+    }
+
+    public void addBack(int num) {
+        if(!set.contains(num)){
+            queue.add(num);
+            set.add(num);
+        }
+    }
+}
