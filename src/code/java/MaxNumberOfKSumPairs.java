5dbda934c6a674688e2fec98af10dd65c40d3e95
gkak12 <hyunjun5681@kakao.com>
2025-08-28 19:06:55 +0900

    Greatest Common Divisor of Strings 코틀린 코드 수정

---
+++ src/code/java/MaxNumberOfKSumPairs.java
@@ -0,0 +1,24 @@
+package code.java;
+
+public class MaxNumberOfKSumPairs {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        int k = 5;
+
+        int result = maxOperations(nums, k);
+        System.out.println(result);
+    }
+
+    public static int maxOperations(int[] nums, int k) {
+        int point1 = 0;
+        int point2 = 1;
+        int len = nums.length;
+
+        while(point1 < len && point2 < len) {
+            
+        }
+
+        return 0;
+    }
+}
