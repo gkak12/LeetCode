adb7f3ac321dc0b2e032270bf731bd0dbe10cb4a
hyunjun <hyunjun5681@kakao.com>
2025-12-28 18:32:31 +0900

    Unique Number of Occurrences 자바 코드 추가

---
+++ src/code/java/UniqueNumberOfOccurrences.java
@@ -0,0 +1,35 @@
+package code.java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/unique-number-of-occurrences/submissions/1865017737/?envType=study-plan-v2&envId=leetcode-75
+ */
+
+public class UniqueNumberOfOccurrences {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,2,1,1,3};
+//        int[] arr = {1,2};
+//        int[] arr = {-3,0,1,-3,1,1,1,-3,10,0};
+
+        boolean result = uniqueOccurrences(arr);
+        System.out.println(result);
+    }
+
+    public static boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int num : arr){
+            int cnt = map.getOrDefault(num, 0) + 1;
+            map.put(num, cnt);
+        }
+
+        long setSize = map.values().stream().distinct().count();    // Map의 Value인 숫자별 개수를 중복 제거하고 Size 조회
+        int keySize = map.keySet().size();  // Map의 Key인 숫자 종류 개수 Size 조회
+
+        boolean result = setSize == keySize;    // setSize와 keySize가 같은 경우 true 리턴, 그렇지 않으면 false 리턴
+        return result;
+    }
+}
