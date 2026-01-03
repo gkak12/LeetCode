0c3eef9607802b7572e57e11db2d387e8dc837bc
hyunjun <hyunjun5681@kakao.com>
2026-01-04 01:19:39 +0900

    Valid Palindrome 투 포인터 적용

--- src/code/java/ValidPalindrome.java
+++ src/code/java/ValidPalindrome.java
@@ -17,10 +17,27 @@ public class ValidPalindrome {
     }
 
     public static boolean isPalindrome(String s) {
-        String str = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();    // 영문자와 숫자를 제외한 나머지 문자를 제거하고 소문자 처리
-        StringBuilder sb = new StringBuilder(str);  // 처리한 문자열을 StringBuilder 객체로 생성
-        String rStr = sb.reverse().toString();      // sb 순서 뒤집고 문자열 반환
+        int leftIdx = 0;
+        int rightIdx = s.length()-1;
 
-        return str.equals(rStr);    // 두 문자열 같은지 비교
+        while(leftIdx < rightIdx) {
+            Character leftC = s.charAt(leftIdx);    // 왼쪽 문자
+            Character rightC = s.charAt(rightIdx);  // 오른쪽 문자
+
+            if(!Character.isLetterOrDigit(leftC)) { // 왼쪽 문자가 영문자나 숫자 아닌 경우
+                leftIdx++;  // 왼쪽 인덱스 증가
+            } else if(!Character.isLetterOrDigit(rightC)) { // 오른쪽 문자가 영문자나 숫자 아닌 경우
+                rightIdx--; // 오른쪽 인덱스 감소
+            } else {    // 왼쪽 문자와 오른쪽 문자가 염문자나 숫자인 경우
+                if(Character.toLowerCase(leftC) != Character.toLowerCase(rightC)) { // 왼쪽 문자와 오른쪽 문자를 소문자로 변환한 뒤 비교해서 다른 경우
+                    return false;   // false 리턴
+                }
+
+                leftIdx++;  // 왼쪽 인덱스 증가
+                rightIdx--; // 오른쪽 인덱스 증가
+            }
+        }
+
+        return true;
     }
 }
