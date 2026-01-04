037a4a736a7db3546ea9c4dd8fd5ab39c758ba05
hyunjun <hyunjun5681@kakao.com>
2026-01-04 20:49:24 +0900

    Combinations 자바 코드 추가

---
+++ src/code/java/Combinations.java
@@ -0,0 +1,38 @@
+package code.java;
+
+/**
+ * https://leetcode.com/problems/combinations/description/?envType=study-plan-v2&envId=top-interview-150
+ */
+
+import java.util.*;
+
+public class Combinations {
+
+    public static void main(String[] args) {
+        int n = 4;
+        int k = 2;
+
+        List<List<Integer>> result = combine(n, k);
+        System.out.println(result);
+    }
+
+    public static List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> resList = new ArrayList<>();
+        search(1, 0, n, k, new ArrayList<>(), resList);
+
+        return resList;
+    }
+
+    public static void search(int num, int depth, int n, int k, List<Integer> curr, List<List<Integer>> list){
+        if(k == depth){     // 모든 숫자를 선택한 경우
+            list.add(new ArrayList<>(curr));    // 현재 선택한 숫자 추가, deep copy
+            return; // 재귀 호출 종료
+        }
+
+        for(int i = num ; i <= n ; i++){    // 숫자 탐색
+            curr.add(i);    // 현재 숫자 추가
+            search(i+1, depth+1, n, k, curr, list); // 현재 다음 숫자 탐색, 재귀 호출
+            curr.removeLast();  // 마지막에 추가한 현재 숫자 삭제
+        }
+    }
+}
