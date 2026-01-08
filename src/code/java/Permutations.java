2eaccd69d8895b1bcbcee40b844f507eeae2f54e
hyunjun <hyunjun5681@kakao.com>
2026-01-09 00:54:00 +0900

    Permutations 스택 적용 수정

--- src/code/java/Permutations.java
+++ src/code/java/Permutations.java
@@ -20,22 +20,22 @@ public class Permutations {
     public static List<List<Integer>> permute(int[] nums) {
         int depth = 0;
         List<List<Integer>> resList = new ArrayList<>();
-        search(depth, nums, new ArrayList<>(),resList);
+        search(depth, nums, new Stack<>(),resList);
 
         return resList;
     }
 
-    public static void search(int depth, int[] nums, List<Integer> curr, List<List<Integer>> resList) {
+    public static void search(int depth, int[] nums, Stack<Integer> stack, List<List<Integer>> resList) {
         if(depth == nums.length){   // 모든 숫자를 선택한 경우
-            resList.add(new ArrayList<>(curr)); // 현재 선택한 숫자들을 resList에 추가, deep copy
+            resList.add(new ArrayList<>(stack)); // 현재 선택한 숫자들을 resList에 추가, deep copy
             return;     // 재귀 호출 종료
         }
 
         for(int i = 0 ; i < nums.length ; i++){ // 숫자 탐색
-            if(!curr.contains(nums[i])){    // 현재 숫자가 curr에 없는 경우
-                curr.add(nums[i]);  // 현재 숫자 curr에 추가
-                search(depth+1, nums, curr, resList);   // 다음 숫자 탐색, 재귀 호출
-                curr.removeLast();  // 현재 숫자 curr에서 제거
+            if(!stack.contains(nums[i])){    // 현재 숫자가 curr에 없는 경우
+                stack.push(nums[i]); // 현재 숫자 curr에 추가
+                search(depth+1, nums, stack, resList);   // 다음 숫자 탐색, 재귀 호출
+                stack.pop();         // 현재 숫자 curr에서 제거
             }
         }
     }
