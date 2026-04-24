7f7150f5af86fae19f47429060ba6936769d157e
gkak12 <hyunjun5681@kakao.com>
2026-04-24 20:56:49 +0900

    Product of Array Except Self 코틀린 코드 주석 추가

--- src/code/kotlin/ProductOfArrayExceptSelf.kt
+++ src/code/kotlin/ProductOfArrayExceptSelf.kt
@@ -1,5 +1,9 @@
 package code.kotlin
 
+/**
+ *  https://leetcode.com/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=leetcode-75
+ */
+
 fun main(){
     val nums = intArrayOf(1,2,3,4)
     val result = productExceptSelf(nums)
@@ -17,26 +21,26 @@ fun productExceptSelf(nums: IntArray): IntArray {
     val len = nums.size
     val result = IntArray(len)
 
-    var zeroCnt = 0
-    var total = 1;
+    var zeroCnt = 0     // 전체 0 개수
+    var total = 1;      // 전체 곱셈값
 
     for(i in 0 until len) {
-        if(nums[i] == 0) {
+        if(nums[i] == 0) {  // 전체 0 개수 카운트
             zeroCnt++
-        } else {
+        } else {    // 0이 아닌 경우 전체 곱셈값 갱신
             total *= nums[i]
         }
     }
 
-    if(zeroCnt > 1) {
-        return result
+    if(zeroCnt > 1) {   // 전체 0 개수가 1 초과인 경우(2개 이상인 경우)
+        return result   // 전체 값이 0인 배열 리턴
     }
 
     for(i in 0 until len) {
-        if(nums[i] == 0) {
-            result[i] = total
-        } else {
-            result[i] = if(zeroCnt == 1) 0 else total/nums[i]
+        if(nums[i] == 0) {      // 현재 배열 요소 값이 0인 경우
+            result[i] = total   // 전체 곱셈값 반영
+        } else {    // 현재 배열 요소 값이 0이 아닌 경우
+            result[i] = if(zeroCnt == 1) 0 else total/nums[i]   // 전체 0 개수가 1이면 0 반영 아니면 전체 곱셈값에서 자기 자신 나눈 값 반영
         }
     }
 
